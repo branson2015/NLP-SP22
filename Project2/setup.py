9e345627e3c834ed9ec2fd61ec72f132ff892596
James Hammer <branson2015@gmail.com>
2022-10-30 15:17:16 -0400

    new history

---
+++ Project2/setup.py
@@ -0,0 +1,5 @@
+#Setup a venv running Python 3.9.0
+#Ex: C:\Users\bbible3\AppData\Local\Programs\Python\Python39\python.exe -m venv .venv
+#Activate the venv
+#Install from requirements.txt
+#Ex: pip install -r requirements.txt
